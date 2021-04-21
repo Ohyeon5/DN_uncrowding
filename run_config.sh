366b985f0c61c547d13e338f280d566c6cb1a955
Oh-hyeon Choung <iohyeonki@gmail.com>
2021-04-21 09:05:23 +0200

    Update run_config.sh

--- run_config.sh
+++ run_config.sh
@@ -5,10 +5,13 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem 8192
+#SBATCH --mem 4096
+#SBATCH --account lpsy
 #SBATCH --time 3:00:00
 
 module load gcc python cuda
 source venvs/torch/bin/activate
+
+git pull https://Ohyeon5:glt55_TT@github.com/Ohyeon5/DN_uncrowding.git
 
 srun python /home/choung/DN_uncrowding/main.py --norm_types ['in','bn','gn','ln']
